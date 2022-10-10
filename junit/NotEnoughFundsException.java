27dcd9572e47984989158054a014e12e6630f164
Shameena-balkees <103920970+Shameena-balkees@users.noreply.github.com>
2022-10-10 17:58:21 +0530

    Add files via upload

---
+++ junit/NotEnoughFundsException.java
@@ -0,0 +1,9 @@
+package activityPrograms;
+
+public class NotEnoughFundsException extends Exception {
+	
+	public NotEnoughFundsException(Integer amount, Integer balance) {
+        super("Attempted to withdraw " + amount + " with a balance of " + balance);
+    }
+
+}
